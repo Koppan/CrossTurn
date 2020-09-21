bb3774d07016ed99c4c20ec1e7c4d7251a771490
Koppan <69089479+Koppan@users.noreply.github.com>
2020-09-21 23:05:00 +0200

    Add files via upload

---
+++ CrossTurn_alpha02/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject enemy;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+    }
+
+    private void moveEnemy(){
+        enemy.transform.position = new Vector3(0, -3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveEnemy();
+    }
+}
